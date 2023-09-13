ad415a534fc3dddd1305d905a0443d3bab0cbf6d
17Sara <87436514+17Sara@users.noreply.github.com>
2023-09-13 22:15:47 +0530

    signup component

--- src/app/pinterest-container/pinterest-container.module.ts
+++ src/app/pinterest-container/pinterest-container.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { PinterestContainerComponent } from './pinterest-container.component';
 import { PinterestTopBarComponent } from './pinterest-top-bar/pinterest-top-bar.component';
 import { PinterestTopBarSigninComponent } from './pinterest-top-bar/pinterest-top-bar-signin/pinterest-top-bar-signin.component';
+import { PinterestTopBarSignupComponent } from './pinterest-top-bar/pinterest-top-bar-signup/pinterest-top-bar-signup.component';
 
 
 
@@ -11,6 +12,7 @@ import { PinterestTopBarSigninComponent } from './pinterest-top-bar/pinterest-to
     PinterestContainerComponent,
     PinterestTopBarComponent,
     PinterestTopBarSigninComponent,
+    PinterestTopBarSignupComponent,
   ],
   imports: [
     CommonModule,
