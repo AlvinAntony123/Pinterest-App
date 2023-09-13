8f73d4501595ddeb772115ac8d9c996f2f4b2ffc
17Sara <87436514+17Sara@users.noreply.github.com>
2023-09-13 21:57:57 +0530

    Revert "new commits"
    This reverts commit 1107edecae314db092c80e3785dd42e46a38774e.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { PinterestContainerModule } from './pinterest-container/pinterest-container.module';
 import { PinterestTopBarComponent } from './pinterest-container/pinterest-top-bar/pinterest-top-bar.component';
 import { PinterestTopBarSigninComponent } from './pinterest-container/pinterest-top-bar/pinterest-top-bar-signin/pinterest-top-bar-signin.component';
-import { PinterestTopBarSignupComponent } from './pinterest-container/pinterest-top-bar/pinterest-top-bar-signup/pinterest-top-bar-signup.component';
 
 @NgModule({
   declarations: [
--- src/app/pinterest-container/pinterest-container.module.ts
+++ src/app/pinterest-container/pinterest-container.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { PinterestContainerComponent } from './pinterest-container.component';
 import { PinterestTopBarComponent } from './pinterest-top-bar/pinterest-top-bar.component';
 import { PinterestTopBarSigninComponent } from './pinterest-top-bar/pinterest-top-bar-signin/pinterest-top-bar-signin.component';
-import { PinterestTopBarSignupComponent } from './pinterest-top-bar/pinterest-top-bar-signup/pinterest-top-bar-signup.component';
 
 
 
@@ -12,7 +11,6 @@ import { PinterestTopBarSignupComponent } from './pinterest-top-bar/pinterest-to
     PinterestContainerComponent,
     PinterestTopBarComponent,
     PinterestTopBarSigninComponent,
-    PinterestTopBarSignupComponent,
   ],
   imports: [
     CommonModule,
