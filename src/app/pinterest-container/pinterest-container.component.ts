58f3ce834b61a91551882245d845847e4518c8b0
Muhammed Faheem Shamsudeen <faheemshams58@gmail.com>
2023-09-14 09:26:57 +0530

    read desc
    1. added like button, for that run //npm install --save font-awesome

--- src/app/pinterest-container/pinterest-container.component.ts
+++ src/app/pinterest-container/pinterest-container.component.ts
@@ -6,9 +6,14 @@ import { ITile } from './pinterest-container.interface';
   templateUrl: './pinterest-container.component.html',
   styleUrls: ['./pinterest-container.component.scss']
 })
-export class PinterestContainerComponent implements OnInit {
-
+export class PinterestContainerComponent implements OnInit 
+{
   public tileList: Array<ITile> = [];
+  isFilled: boolean = false;
+
+  toggleHeart() {
+    this.isFilled = !this.isFilled;
+  }
 
   public ngOnInit(): void {
     this.tileList = [
@@ -17,7 +22,13 @@ export class PinterestContainerComponent implements OnInit {
       {id:3, label:'Tile 3', isFavourite: false, imgUrl:"https://th.bing.com/th/id/OIP.rvSWtRd_oPRTwDoTCmkP5gHaE8?w=227&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"},
       {id:4, label:'Tile 4', isFavourite: false, imgUrl:"https://th.bing.com/th/id/OIP.rvSWtRd_oPRTwDoTCmkP5gHaE8?w=227&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"},
       {id:5, label:'Tile 5', isFavourite: false, imgUrl:"https://th.bing.com/th/id/OIP.rvSWtRd_oPRTwDoTCmkP5gHaE8?w=227&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"},
+      {id:6, label:'Tile 6', isFavourite: false, imgUrl:"https://th.bing.com/th/id/OIP.rvSWtRd_oPRTwDoTCmkP5gHaE8?w=227&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"},
+      {id:7, label:'Tile 7', isFavourite: false, imgUrl:"https://th.bing.com/th/id/OIP.rvSWtRd_oPRTwDoTCmkP5gHaE8?w=227&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"},
+      {id:8, label:'Tile 8', isFavourite: false, imgUrl:"https://th.bing.com/th/id/OIP.rvSWtRd_oPRTwDoTCmkP5gHaE8?w=227&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"},
     ]
   }
 
 }
+
+
+//npm install --save font-awesome
